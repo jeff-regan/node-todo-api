229e4b19bf9b7317ded5c8e55fccd16dab971d22
jeff regan <jeff@jkregan.com>
2018-05-12 12:38:55 -0600

    added get todos by ID and tests for the same

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { mongoose } = require('./db/mongoose');
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./db/mongoose');
 
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
@@ -30,6 +31,23 @@ app.get('/todos', (req, res) => {
 			res.send({todos});
 		}, (e) => {
 			res.status(400).send(e);
+		});
+});
+
+app.get('/todos/:id', (req, res) => {
+	const id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	Todo.findById(id)
+		.then((todo) => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+			res.send({todo});
+		}).catch((e) => {
+			res.status(400).send();
 		});
 });
 
